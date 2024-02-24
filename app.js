35f29c16e0298bf78804a148ba7856efe8f1b579
saadahsan2023 <saadahsankhan890@gmail.com>
2024-02-24 11:33:29 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,130 @@
+// var giveNumber = prompt("Enter your number");
+// var congiveNumber = parseInt(giveNumber);
+
+// if (congiveNumber % 5 === 0) {
+//     console.log("yes ye ata 5 k table me");
+
+
+// } else if (congiveNumber % 2 === 0) {
+//     console.log("yes ye 2 me ata ha");
+// } else if (congiveNumber % 3 === 0) {
+//     console.log("yes ye 3 me ata ha");
+// }
+// else if (congiveNumber % 4 === 0) {
+//     console.log("yes ye 4 me ata ha");
+// } else if (congiveNumber % 6 === 0) {
+//     console.log("yes ye 6 me ata ha");
+// }
+// else if (congiveNumber % 7 === 0) {
+//     console.log("yes ye 7 me ata ha");
+// } else if (congiveNumber % 8 === 0) {
+//     console.log("yes ye 8 me ata ha");
+// }
+// else if (congiveNumber % 9 === 0) {
+//     console.log("yes ye 9 me ata ha");
+// } else if (congiveNumber % 10 === 0) {
+//     console.log("yes ye 10 me ata ha");
+// }
+// else {
+//     alert("enter your number")
+
+// }
+// console.log(` this is your final ans brother: ${congiveNumber}`);
+// let num = prompt("ENTER any number");
+// let secondNum = parseInt(num);
+// if(secondNum<=100 && secondNum>=80){
+//     console.log("A");
+
+// }else if(secondNum<=79 && secondNum >=60){
+// console.log("B");
+// }
+// else if(secondNum <=59 && secondNum >=40){
+// console.log("c");
+
+// }else if(secondNum <=39 && secondNum >=20){
+// console.log("d");
+
+// }else if(secondNum <=19){
+// console.log("fails");
+
+// }else{
+//     console.log("enter number ppliz");
+
+// }
+// let sum = 0;
+// let givenValue = 30;
+// for (let i = 1; i <= givenValue; i++) {
+//     sum  = sum + i;
+
+// }
+// console.log(sum);
+
+
+
+// for(let even = 1; even <= 50; even++){
+//     console.log(even*3);
+
+// }
+
+
+
+// let matchScore = 30;
+// let startGame = prompt("enter coreect number");
+
+// while (startGame != matchScore) {
+//     let startGame = prompt("enter coreect number");
+
+// }
+// console.log("apky number sahi ha bahi");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
